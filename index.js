72743bceacc1a6688c45dc9fa74e2ea7075bdcaf
Aiden Wahed <prismhead26@gmail.com>
2024-02-15 16:20:54 -0700

    chalk implemented...terminal output completed

--- index.js
+++ index.js
@@ -6,55 +6,54 @@ const { error } = require('console');
 const chalk = require('chalk')
 
 // const chalkPipe = require('chalk-pipe');
-
 // Array of questions for user input
 const questions = [
         {
             name: "title",
-            message: "What is the name of your Project Title?",
-            default: 'cyan'
+            message: chalk.blue('What is the name of your Project Title?'),
+        
         },
         {
             name: 'username',
-            message: 'Provide your full name for coypright.'
+            message: chalk.blue('Provide your full name for coypright.')
         },
         {
             name: 'description',
-            message: 'Provide a short description explaining the what, why, and how of your project.'
+            message: chalk.blue('Provide a short description explaining the what, why, and how of your project.')
         },
         {
             name: 'installation',
-            message: 'Provide instructions and examples for use. Include screenshots as needed.  To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax: ![alt text](assets/images/screenshot.png)'
+            message: chalk.blue('Provide instructions and examples for use. Include screenshots as needed.  To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax: ![alt text](assets/images/screenshot.png)')
         },
         {
             name: 'usage',
-            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+            message: chalk.blue('What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'),
         },
         {
             name: 'contribution',
-            message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.',
+            message: chalk.blue('If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.'),
         },
         {
             name: "test",
-            message: "Go the extra mile and write tests for your application. Then provide examples on how to run them here.",
+            message: chalk.blue('Go the extra mile and write tests for your application. Then provide examples on how to run them here.'),
         },
         {
             type: "list",
             name: "license",
-            message: "Choose a license for your project.",
+            message: chalk.blue("Choose a license for your project."),
             choices: ['The MIT License', 'Apache 2.0 License', 'GNU GPL v3', 'Mozilla Public License 2.0', 'BSD 3-Clause License', 'none']
         },
         {
             name: "github",
-            message: "Create a link by providing your GitHub profile name.",
+            message: chalk.blue('Create a link by providing your GitHub profile name.'),
         },
         {
             name: "email",
-            message: "Provide a link to your email for questions.",
+            message: chalk.blue('Provide a link to your email for questions.'),
         },
         {
             name: "features",
-            message: "If your project has a lot of features, list them here.",
+            message: chalk.blue('If your project has a lot of features, list them here.'),
         }
 ];
 // Function writes README file
@@ -67,7 +66,7 @@ function writeToFile(fileName, data) {
 }
 // Function initializes app .... using async/await rather than .then/catch
 async function init() {
-    console.log(chalk.bgYellowBright.black('Welcome... professional READme generator is spooling up.'))
+    console.log(chalk.bgMagenta.black('Welcome... professional READme generator is spooling up.'))
 
     const answers = await inquirer.prompt(questions)
 
